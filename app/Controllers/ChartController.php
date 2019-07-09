485270595c728f94d981b23b4354a888a96b52b2
tunahanertekin <tertekin@etu.edu.tr>
2019-07-09 16:34:45 +0300

    Old version is cleaned, form choice is added. Must adjust headers.

--- app/Controllers/ChartController.php
+++ app/Controllers/ChartController.php
@@ -6,37 +6,68 @@ use PDO;
 use App\Models\JotForm;
 use Interop\Container\ContainerInterface;
 
+/*
+form ve submissions her seferinde API'den alınıyor, problem.
+*/
+
+/*
+Function Design:
+--Catch and assign
+--Create an information format
+--Send it to frontend.
+*/
 
 class ChartController{
 
-    protected $jotformAPI;
-    public $form;
-    public $submissions;
-    public $c;
+    protected $jotformAPI; //API connection variable
+    public $form; //Form choosed
+    public $submissions; //Submissions of form
+    public $c; //Container variable
 
     public function __construct(ContainerInterface $c){
         $this->c = $c;
         $this->jotformAPI = new JotForm("286dec88b006d9221daf40d94278c162");
-
-        $forms = $this->jotformAPI->getForms();
-       
-        foreach($forms as $f){
-            if($f["title"] == "Job Application"){
-                $this->form = $f;
-            }
-        }
-
-       
-        $this->submissions = $this->jotformAPI->getFormSubmissions($this->form["id"]);
-
     }
 
+    public function frontendTrial($request,$response,$args){
+        return $this->c->view->render($response,"designed/frontend.twig");
+    }
+
+    /*
+    * For every request, data is received from API.
+    */
+    public function setFormAndSubmissions($id){
+        $this->form = $this->jotformAPI->getForm($id);
+        $this->submissions = $this->jotformAPI->getFormSubmissions($id);
+    }
+
+    /*
+    * User must choose a form. Lists all forms.
+    */
+    public function formChoice($request,$response,$args){
+        $forms = $this->jotformAPI->getForms();
+        
+        $formArr = array();
+        foreach($forms as $f){
+            array_push($formArr,array($f["id"],$f["title"],$f["count"],$f["updated_at"]));
+        }
+
+        return $this->c->view->render($response,"designed/form_choice.twig",compact("formArr"));
+    }
+
+    /*
+    * Three stat-format is available. This route/function shows all formats to user and receive an answer.
+    * User chooses what type of stats will be shown and selects the question/s.
+    */
     public function questionChoice($request,$response,$args){
+        
+        $formID = $args["formID"];
+        $this->setFormAndSubmissions($formID);
+        $formName = $this->jotformAPI->getForm($formID)["title"];
         $questions = $this->jotformAPI->getFormQuestions($this->form["id"]);
         
         $infoArr = array();
         $allQuestions = array();
-
         foreach($questions as $q){
             if($q["type"] == "control_checkbox"){
                 array_push($infoArr,array($q["qid"],$q["text"]));
@@ -46,84 +77,91 @@ class ChartController{
             }
         }
 
-        return $this->c->view->render($response,"designed/choice_page.twig",compact("infoArr","allQuestions"));
+        return $this->c->view->render($response,"designed/choice_page.twig",compact("infoArr","allQuestions","formID","formName"));
     }
 
+    /*
+    * Simple pie chart and radar chart is shown. 
+    * Features are explained.
+    */
     public function index($request,$response,$args){
         
+        $formID = $args["formID"];
+        $this->setFormAndSubmissions($formID);
         $qid = $args["mcq"];
         $questionJSON = $this->jotformAPI->getFormQuestion($this->form["id"],$qid);
-
+        
         $submissionArr = array();
-
         foreach($this->submissions as $s){
             array_push($submissionArr,array($s["created_at"],$s["answers"][$qid]["answer"],$s["id"]));
         }
-
         $question = array($questionJSON["qid"],str_ireplace("'","",$questionJSON["text"]),$this->giveOptionsArray($questionJSON["options"]));
+        
         return $this->c->view->render($response,"designed/index.twig",compact("submissionArr","question"));
     }
 
+    /*
+    * Sends the submission data to frontend, then filter it with JS. Time periods are compared.
+    */
     public function timeBasis($request,$response,$args){
         
+        $formID = $args["formID"];
+        $this->setFormAndSubmissions($formID);
         $qid = $args["mcq"];
         $questionJSON = $this->jotformAPI->getFormQuestion($this->form["id"],$qid);
 
         $submissionArr = array();
-
         foreach($this->submissions as $s){
             array_push($submissionArr,array($s["created_at"],$s["answers"][$qid]["answer"],$s["id"]));
         }
-
         $question = array($questionJSON["qid"],str_ireplace("'","",$questionJSON["text"]),$this->giveOptionsArray($questionJSON["options"]));
         
         return $this->c->view->render($response,"designed/time_basis.twig",compact("question","submissionArr"));
     }
 
+    /*
+    * This feature is for every basic form elements. Scoped for MC question, other questions' stats are shown
+    * with charts and tables.
+    */
     public function relatedStats($request,$response,$args){
-
+        
+        $formID = $args["formID"];
+        $this->setFormAndSubmissions($formID);
         $qid = $args["mcq"];
         $questionJSON = $this->jotformAPI->getFormQuestion($this->form["id"],$qid);
-
         $questionNumber = sizeof($this->jotformAPI->getFormQuestions($this->form["id"]));
-
         $otherQid = $args["oq"];
         $otherQuestionJSON = $this->jotformAPI->getFormQuestion($this->form["id"],$otherQid);
-
         $otherQuestionType = $otherQuestionJSON["type"];
 
         $submissionArr = $this->getAuxSubmissionArr($qid,$otherQid);
-
-        $formID = $this->form["id"];
-
         $question = array($questionJSON["qid"],str_ireplace("'","",$questionJSON["text"]),$this->giveOptionsArray($questionJSON["options"]));
         $otherQuestion = array($otherQuestionJSON["qid"],str_ireplace("'","",$otherQuestionJSON["text"]),$otherQuestionJSON["type"]);
 
         return $this->c->view->render($response,"designed/related_stats.twig",compact("submissionArr","question","otherQuestion","formID"));
     }
 
+    /*
+    * Simplifies the submission data.
+    */
     public function getAuxSubmissionArr($mcQid,$otherQid){
         $compactArr = array();
         /*
             It is convenient for 
             all basic form elements.
-
             ***Problem with apostrophe in JSON parsing!
         */
-        
         foreach($this->submissions as $s){
             if(array_key_exists("answer",$s["answers"][$otherQid])){
-                echo "hey";
+                
                 if(array_key_exists("answer",$s["answers"][$mcQid])){
                     $s["answers"][$otherQid]["answer"] = str_ireplace("'","",$s["answers"][$otherQid]["answer"]);
                     array_push($compactArr,array($s["created_at"],$s["answers"][$mcQid]["answer"],$s["answers"][$otherQid]["answer"],$s["id"]));
-                }
-                    
+                }   
                 else{
                     $s["answers"][$otherQid]["answer"] = str_ireplace("'","",$s["answers"][$otherQid]["answer"]);
                     array_push($compactArr,array($s["created_at"],array(),$s["answers"][$otherQid]["answer"],$s["id"]));
                 }
-                    
             }
         }
        
@@ -142,5 +180,4 @@ class ChartController{
 
         return $options;
     }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,21 @@
 <?php
 
-
 use App\Middleware\DefaultMiddleware;
-
-use App\Models\MultipleChoices;
-use App\Models\SpecializedData;
 
 use App\Middleware\RedirectIfUnauthenticated;
 use Interop\Container\ContainerInterface;
 
 use App\Controllers\ChartController;
 
-
-$app->get('/',function($request,$response,$args){
-    return "Main page";
-});
-
-/*
-$app->get('/display',function($request,$response,$args){
-    return $this->view->render($response, 'display.twig');
-    
-});
-*/
-
-
-$app->group('/multiple-choices',function(){
-    $this->get('',MultipleChoices::class.":index");
-    $this->post('',MultipleChoices::class.":index");
-
-    $this->group('/detailed',function(){
-        $this->get('',MultipleChoices::class.":detailedIndex");
-        $this->post('',MultipleChoices::class.":detailedIndex");
-        $this->group('/{qid}',function(){
-            $this->post('',MultipleChoices::class.":seeAllAttributes");
-            $this->get('',MultipleChoices::class.":seeAllAttributes");
-            $this->get('/radar',MultipleChoices::class.":radarCompare");
-            $this->post('/radar',MultipleChoices::class.":radarCompare");
-        });
-    });
-
-    $this->group('/specialized',function(){
-        $this->get('',SpecializedData::class.":index");
-        $this->post('',SpecializedData::class.":otherQuestions");
-
-        $this->post('/{qid}',SpecializedData::class.":dynamicStats");
-    });
-    
-});
-
-$app->get('/example',ChartController::class.":index");
-
-
 $app->group('/jotcharts',function(){
     
-    $this->get('',ChartController::class.":questionChoice");
+    $this->get('',ChartController::class.":formChoice");
         
     $this->group('/{formID}',function(){
+
+        $this->get('',ChartController::class.":questionChoice");
+        $this->post('',ChartController::class.":questionChoice");
+
         $this->group('/bar-radar',function(){
             $this->get('/{mcq}',ChartController::class.":index");
             $this->post('/{mcq}',ChartController::class.":index");
@@ -72,3 +32,5 @@ $app->group('/jotcharts',function(){
     
    
 });
+
+$app->get('/frontend',ChartController::class.":frontendTrial");
